effd5675f27655e2943f8c6ce15d441a9cd2873a
Jumanazar04 <yusupovjumanazar004@gmail.com>
2024-01-18 13:13:49 +0500

    end

--- app.js
+++ app.js
@@ -81,7 +81,7 @@ function checkWinner(){
     for(let c=0; c<3; c++){
         if(board[0][c] == board[1][c] && board[1][c] == board[2][c] && board[0][c] != ' '){
             for(let i=0; i<3; i++){
-                let tile = document.getElementById(c.toString() + '-' + i.toString())
+                let tile = document.getElementById(i.toString() + '-' + c.toString())
                 tile.classList.add('winner')
             }
             gameOver = true;
